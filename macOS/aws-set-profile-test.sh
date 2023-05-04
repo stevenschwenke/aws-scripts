1f38fabc555dfc51b3a2de34f29c0c2c735e8dd6
Steven Schwenke <steven@stevenschwenke.de>
2023-05-04 15:06:59 +0200

    Reformat test cases so that description is the first parameter

--- macOS/aws-set-profile-test.sh
+++ macOS/aws-set-profile-test.sh
@@ -7,10 +7,10 @@ ACTUAL_PROFILES=$1
 
 # Function to test the output of aws-set-profile.sh
 function test_myscript() {
-  local run_with_source=$1
-  local input=$2
-  local expected_output=$3
-  local description=$4
+  local description=$1
+  local run_with_source=$2
+  local input=$3
+  local expected_output=$4
 
   local raw_actual_output
   if [[ "$run_with_source" == "source" ]]; then
@@ -35,74 +35,72 @@ function test_myscript() {
 }
 
 # Test cases
-test_myscript "nosource" \
+test_myscript "Missing parameter (without source)" \
+              "nosource" \
               "" \
               "Please call this script source'd like this to make your changes permanent:
                   source ./aws-set-profile.sh
                   Or like this:
                   . ./aws-set-profile.sh
-                  The environment was not changed." \
-                  "Missing parameter (without source)"
+                  The environment was not changed."
 
-test_myscript "source" \
+test_myscript "Missing parameter (with source)" \
+              "source" \
               "" \
-              "Profile name is missing. Please provide a profile name as an argument." \
-                  "Missing parameter (with source)"
+              "Profile name is missing. Please provide a profile name as an argument."
 
 # TODO Should work even without source
-test_myscript "nosource" \
+test_myscript "Show help (without source)" \
+              "nosource" \
               "-h" \
               "Please call this script source'd like this to make your changes permanent:
                     source ./aws-set-profile.sh
                     Or like this:
                     . ./aws-set-profile.sh
                     The environment was not changed." \
-              "Show help (without source)"
 
-test_myscript "source" \
+test_myscript "Show help (with source)" \
+              "source" \
               "-h" \
               "This will help you set an AWS profile.
 
                -l   list all your profiles
                -h   display help
-               name set a profile with a name" \
-              "Show help (with source)"
+               name set a profile with a name"
 
 # TODO Should work even without source
-test_myscript "nosource" \
+test_myscript "List profiles (without source)" \
+              "nosource" \
               "-l" \
               "Please call this script source'd like this to make your changes permanent:
                     source ./aws-set-profile.sh
                     Or like this:
                     . ./aws-set-profile.sh
-                    The environment was not changed." \
-              "List profiles (without source)"
+                    The environment was not changed."
 
-test_myscript "source" \
+test_myscript "List profiles (with source)" \
+              "source" \
               "-l" \
               "Your AWS profiles:
 
-              $ACTUAL_PROFILES" \
-              "List profiles (with source)"
+              $ACTUAL_PROFILES"
 
-test_myscript "nosource" \
+test_myscript "Set profile (without source)" \
+              "nosource" \
               "my_profile" \
               "Please call this script source'd like this to make your changes permanent:
                     source ./aws-set-profile.sh
                     Or like this:
                     . ./aws-set-profile.sh
-                    The environment was not changed." \
-              "Set profile (without source)"
+                    The environment was not changed."
 
-test_myscript "source" \
+test_myscript "Set profile (with source)" \
+              "source" \
               "my_profile" \
               "Setting profile with name my_profile:
 
                Executing 'env | grep AWS_PROFILE':
                AWS_PROFILE=my_profile
 
-               You may also call 'aws sts get-caller-identity' to show your AWS account ID." \
-              "Set profile (with source)"
+               You may also call 'aws sts get-caller-identity' to show your AWS account ID."
 
-# Add more test cases as needed
-#test_myscript "input1" "expected_output1" "Test case 1 description"
